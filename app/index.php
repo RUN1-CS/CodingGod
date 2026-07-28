1d9fb7f8ba069dec171cd859610c8d1bd856ca23
Martincz7578 <martinek@rychly.cz>
2026-07-28 15:09:41 +0200

    Minor changes, it builds but ain't working as should.

--- app/index.php
+++ app/index.php
@@ -12,7 +12,7 @@ if(isset($_POST['theme'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CodingGod</title>
+    <title>The Economy Sim</title>
     <link rel="stylesheet" href="/game.css">
 </head>
 <script type="module" src="/Scripts/game.js"></script>
